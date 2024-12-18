c669c5769caca39391304205b8a18be130411507
Eveline <evelinesantiago@alu.ufc.br>
2024-12-18 19:57:45 -0300

    up

--- StartService/app/src/main/java/com/example/startservice/ServiceControlScreen.kt
+++ StartService/app/src/main/java/com/example/startservice/ServiceControlScreen.kt
@@ -1,4 +1,48 @@
 package com.example.startservice
 
-class ServiceControlScreen {
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.ui.*
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ServiceControlScreen(
+    onStartClick: () -> Unit,
+    onStopClick: () -> Unit
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = onStartClick,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Iniciar Serviço")
+            }
+
+            Button(
+                onClick = onStopClick,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Parar Serviço")
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ver() {
+    ServiceControlScreen(
+        onStartClick = {},
+        onStopClick = {}
+    )
 }
