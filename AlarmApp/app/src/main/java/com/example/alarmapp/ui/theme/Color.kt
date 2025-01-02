fb900205ff625e17ea4435ed82df2015c9c5bb0a
Eveline <evelinesantiago@alu.ufc.br>
2025-01-02 10:58:24 -0300

    add AlarmApp

---
+++ AlarmApp/app/src/main/java/com/example/alarmapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.alarmapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF00B6D5)
+val PurpleGrey80 = Color(0xFFCEC0E1)
+val Pink80 = Color(0xFF8DD3FF)
+
+val Purple40 = Color(0xFF005BA4)
+val PurpleGrey40 = Color(0xFF7E72AB)
+val Pink40 = Color(0xFFB980C2)
