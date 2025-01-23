2203fc13429e129526b5749bc0ec3949f5ffb020
Eveline <evelinesantiago@alu.ufc.br>
2025-01-23 18:41:24 -0300

    msgapp completo

--- MsgApp/app/src/main/java/com/example/msgapp/MainActivity.kt
+++ MsgApp/app/src/main/java/com/example/msgapp/MainActivity.kt
@@ -3,45 +3,32 @@ package com.example.msgapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Room
+import com.example.msgapp.data.local.database.AppDatabase
+import com.example.msgapp.repository.MessageRepository
 import com.example.msgapp.ui.theme.MsgAppTheme
+import com.example.msgapp.ui.theme.view.MessageApp
+import com.example.msgapp.viewmodel.MessageViewModel
+import com.example.msgapp.viewmodel.MessageViewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "messages-db"
+        ).fallbackToDestructiveMigration().build()
+        val repository = MessageRepository(db.messageDao())
+
         setContent {
             MsgAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel: MessageViewModel =
+                    viewModel(factory = MessageViewModelFactory(repository))
+                MessageApp(viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MsgAppTheme {
-        Greeting("Android")
     }
 }
